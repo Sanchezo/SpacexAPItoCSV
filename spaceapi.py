6d581af0e099efc86d498c05dd0762ca54c4bc93
Sanchezo <48131180+Sanchezo@users.noreply.github.com>
2021-04-24 23:18:26 +0200

    Update spaceapi.py

--- spaceapi.py
+++ spaceapi.py
@@ -11,14 +11,16 @@ def append_list_as_row(file_name, list_of_elem): #We create a function that allo
 apiendpoint="https://api.spacexdata.com/v3/launches" #API URL
 response = requests.get(apiendpoint) #We request data from API
 data = response.json() #We convert data that we received from API to a JSON file
+
 for row in response.json(): #We convert the rows from JSON file into strings
     f_number = row.get('flight_number', None)
     name = row.get('mission_name', None)
-    craft_id = row.get('rocket_id', None)
-    craft_name = row.get('rocket_name', None)
+    craft = row.get('rocket', None)
+    patch=row.get("links",None)
+   
     date_utc = row.get('launch_date_utc', None)
-    link = row.get('video_link', None)
-    data_list=[f_number,name,craft_id,craft_name,date_utc,link] #We put the strings into a list
+    
+    data_list=[f_number,name,craft.get('rocket_id',None),craft.get('rocket_name',None),patch.get('video_link',None),date_utc] #We put the strings into a list
     append_list_as_row('flights.csv', data_list) #We save the list as a row in CSV
 #Made by Jakub Kapidura with the help of resources from the Internet
     
